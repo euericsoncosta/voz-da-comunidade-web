ce0e7c3094395f884ca539c3f074b1443d0e2dce
euericsoncosta <costaericson@hotmail.com>
2026-05-21 11:07:43 -0300

    conclusão do build e ajustes de estilo

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // ou @vitejs/react-refresh conforme seu projeto
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/voz-da-comunidade-web/', // ← Use exatamente o nome do repositório entre as barras
+  base: '/voz-da-comunidade-web/', // ← Nome exato do seu repositório no GitHub
 })
-```
-
-### 🌍 Como vai ficar o seu link público na nuvem?
-Assim que você rodar o comando "npm run deploy" no terminal do VS Code, o GitHub Pages vai colocar o seu front-end no ar e o link oficial para colocar dentro do **MIT App Inventor** ou abrir no navegador será:
